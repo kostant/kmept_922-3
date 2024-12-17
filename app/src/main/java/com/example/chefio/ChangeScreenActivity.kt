a1b4eb157ce1a781e8cdd749bee4b14e8a3439e5
Pncheeck <polyakov.vlad2020@yandex.ru>
2024-12-17 15:07:00 +0300

    change screen

--- app/src/main/java/com/example/chefio/ChangeScreenActivity.kt
+++ app/src/main/java/com/example/chefio/ChangeScreenActivity.kt
@@ -1,6 +1,5 @@
 package com.example.chefio
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,19 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ChangeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val ChangeButton: Button = findViewById(R.id.polyakov_button)
-        ChangeButton.setOnClickListener{
-            val intent = Intent(this, ChangeScreenActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.polyakov_screen)
 
     }
-
 
 
 }
