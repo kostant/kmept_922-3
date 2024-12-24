0eb59dec8f69403322082df744feaa7041733fc4
EHoTiNKA <alexe.pyshkin@yandex.ru>
2024-12-24 12:42:55 +0300

    addSecondWindow

--- app/src/main/java/com/example/chefio/MainActivity.kt
+++ app/src/main/java/com/example/chefio/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chefio
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var pyshkinButton: Button = findViewById(R.id.randome_doge)
+
+        pyshkinButton.setOnClickListener() {
+            val intent = Intent(this, PyshkinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/chefio/PyshkinActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chefio
+import androidx.appcompat.app.AppCompatActivity
+class PyshkinActivity: AppCompatActivity() {
+
+}
