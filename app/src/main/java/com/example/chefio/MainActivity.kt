b21b88b00790c4b5060474602c15304134b1cbe1
Konstantin Antonchik <kostant12@gmail.com>
2024-12-24 12:43:33 +0300

    fix errors

--- app/src/main/java/com/example/chefio/MainActivity.kt
+++ app/src/main/java/com/example/chefio/MainActivity.kt
@@ -3,18 +3,8 @@ package com.example.chefio
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Telephony.Mms.Intents
-import android.content.Intent
-import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import com.example.chefio.R
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
