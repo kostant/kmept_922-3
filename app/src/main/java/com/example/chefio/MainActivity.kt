c999cdfc02e240755d11c51d276e38223b52f232
Konstantin Antonchik <kostant12@gmail.com>
2024-12-24 12:49:30 +0300

    fix errors

--- app/src/main/java/com/example/chefio/MainActivity.kt
+++ app/src/main/java/com/example/chefio/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.chefio
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
